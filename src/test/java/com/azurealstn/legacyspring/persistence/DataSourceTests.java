9579d4d91585f8ca23c4665c9cf392ba0c88cb6f
azurealstn <azurealstn@naver.com>
2021-08-14 15:23:49 +0900

    커넥션풀, myBatis 연동

---
+++ src/test/java/com/azurealstn/legacyspring/persistence/DataSourceTests.java
@@ -0,0 +1,48 @@
+package com.azurealstn.legacyspring.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.Assert.fail;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring-servlet.xml")
+@Log4j
+public class DataSourceTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void testConnection() {
+        try (Connection conn = dataSource.getConnection()) {
+            log.info(conn);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMyBatis() {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession();
+            Connection conn = sqlSession.getConnection();
+        ) {
+            log.info(sqlSession);
+            log.info(conn);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
